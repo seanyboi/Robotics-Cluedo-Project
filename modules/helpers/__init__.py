10c68b1b939246eba5f9a24bfee30f01f5cfc7df
Ilyass Taouil <sc14it@leeds.ac.uk>
2017-11-24 16:39:30 +0000

    Integrating ar centering.

---
+++ modules/helpers/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Export files
+from common import *
+from plane_tracker import *
+from processing import *
+from video import *
+from position import *
+from go_to_pose import *
---
+++ modules/helpers/processing.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# ROS and OpenCV packages
+from __future__ import division
+import cv2
+
+# Blur the image
+def blur(img):
+    return cv2.medianBlur(img, 5)
+
+# Convert to greyscale
+def greyscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Adaptive thresholding
+def threshold(img):
+    return cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 10)
+
+# Morphological closing
+def morph(img):
+    return cv2.morphologyEx(img, cv2.MORPH_OPEN, (5,5))
+
+# Canny edge detector
+def canny(img):
+    return cv2.Canny(img, 100, 200)
